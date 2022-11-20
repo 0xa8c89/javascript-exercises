9df2198f11a0ea11d507702b9ab4c8492f0b3785
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-21 00:38:40 +0100

    Do exc 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    if (string.length === 1) return true;
 
+    else if (string.length % 2 === 0) {
+        const center = string.length/2;
+            if(string.slice(0, center).split('').reverse().join('') === string.slice(center))
+                return true;
+            return false;
+    }
+    else {
+        const center = Math.floor(string.length/2);
+        if(string.slice(0, center).split('').reverse().join('') === string.slice(center+1))
+            return true;
+        return false;
+    }
 };
 
 // Do not edit below this line
