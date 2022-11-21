ecf69f8905b5a3f8c402eee1513f47a39cd5e396
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-21 01:36:41 +0100

    Do exc 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 0) return "OOPS";
+    let previousNumber = 0;
+    let currentNumber = 1;
 
+    for (let i = 0; i < num; ++i) {
+        let temp = currentNumber;
+        currentNumber += previousNumber;
+        previousNumber = temp;
+    }
+    return previousNumber;
 };
 
 // Do not edit below this line
