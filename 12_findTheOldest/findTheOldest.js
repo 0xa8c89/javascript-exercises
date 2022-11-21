fa909f7697e22b0bf2cc6c5279fbb8f481d74641
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-21 01:59:54 +0100

    Do exc 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((a, b) => {
+        (a.yearOfBirth - a.yearOfDeath) - (b.yearOfBirth - b.yearOfDeath) ? 1 : -1
+    })
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+// module.exports = findTheOldest;
